b1f91fa5d50493929de2d233cfd50cfcc9627895
Jessica Alin Gonzalez <jag816@gmail.com>
2020-05-21 16:13:49 -0400

    add template pages

--- functions.php
+++ functions.php
@@ -1,7 +1,39 @@
 <?php
 
+// Add CSS File
+
 function mfc_resources() {
   wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'mfc_resources');
+
+// Navigation Menus
+
+register_nav_menus(array(
+  'header' => __('Header Menu'),
+  'footer' => __('Footer Menu')
+));
+
+// Get Top Ancestor
+
+function get_top_ancestor_id() {
+  global $post;
+  if ($post->post_parent) {
+    $ancestors = array_reverse(get_post_ancestors($post->ID));
+    return $ancestors[0];
+  }
+  return $post->ID;
+}
+
+// Check if Page Has Children
+
+ function has_children() {
+  // make the post var accesiible within this function
+  global $post;
+  // get_pages (WP)
+  // get any pages that are a child of the current page
+  $pages = get_pages('child_of=' . $post->ID);
+  return count($pages);
+
+}
--- header.php
+++ header.php
@@ -16,6 +16,19 @@
         <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
       </h1>
       <h5><?php bloginfo('description'); ?></h5>
+      <!-- contional for adding content specific to a page -->
+      <?php if(is_page('make-a-difference')) { ?>
+        <p>Make a Difference</p>
+      <?php } ?>
+      <!-- site navigation -->
+      <nav class="site-nav">
+        <?php
+          $args = array(
+            'theme_location' => 'header'
+          );
+        ?>
+        <?php wp_nav_menu( $args ); ?>
+      </nav>
     </header>
     <!-- /site header -->
   
